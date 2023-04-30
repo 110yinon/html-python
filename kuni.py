d7d37f7ad0db2a6b36b86ab765ef7926272c7b1c
yinon@litepoint <yinon@litepoint>
2023-04-30 12:55:08 +0300

    sub test summary initialy work

--- kuni.py
+++ kuni.py
@@ -30,11 +30,89 @@ for row in rows:
     verdict = 'PASS' if 'Pass' in row else 'FAILED'
 
     if 'Flow File' in row:
+        # print(row)
+
+        # removing the 'running ...[] false\n' (the first line)
+        # adding back the '1.' after remove it on split
+        offTrailingRow = row.split(' 1.',1)[1].replace(' ', '  1. ',1)
+        # print(sara)
+
+        summaryContent = []
+
+        # split into sub tests
+        subTests = re.split(r" \d\. ",offTrailingRow)
+        for subTest in subTests:
+            # print(subTest)
+
+            subTestContent = []
+            subTestHead = []
+            categoriesContent = []
+            valuesContent = []
+
+            # split the subTest paragrph into rows
+            subTestRows = subTest.split('\n')
+            for row in subTestRows:
+                # print(f'x--- {row}')
+                
+                isVerdict = 'PASS' in row or 'FAILED' in row
+                isVerticalBar = '|' in row
+
+                testHeaderRow = isVerdict and not isVerticalBar
+                testCategoriesRow = isVerdict is False and isVerticalBar
+                testValuesRow = isVerdict is True and isVerticalBar
+                
+                if testHeaderRow:
+                    # print(f'x--Header-- {row}')
+                    rowVerdict = 'PASS' if 'PASS' in row else 'FAILED'
+                    subTestName = row.split(rowVerdict)[0]
+                    subTestHead = [                        
+                        div(class_='sub-test-name')[subTestName],
+                        div(class_='verdict')[rowVerdict]                        
+                    ]
+
+                if testCategoriesRow:
+                    # print(f'x--CAT-- {row}')
+                    # seperates the categories in the row
+                    categories = row.split('|')
+                    
+                    for cat in categories:
+                        catName = cat.strip()
+                        categoriesContent += [
+                                div(class_='category')[catName]
+                        ]
+
+                if testValuesRow:
+                    # print(f'x--VAL-- {row}')
+                    # seperates the values in the row
+                    values = row.split('|')
+                    
+                    for val in values:
+                        valuesContent += [
+                                div(class_='value')[val.strip()]
+                        ] 
+
+
+                subTestContent = [
+                    div(class_='sub-test')[
+                        *subTestHead,
+                        div(class_='iterates-categories')[*categoriesContent],
+                        div(class_='iterate-values')[*valuesContent]
+                    ]
+                ]
+                summaryContent += [*subTestContent]
+
+                subTestHead=[]
+                categoriesContent=[]
+                valuesContent=[]
+                subTestContent = []
+                continue
+
+        
         content += [div(class_='flow-test')[
                 div(class_='test-name')[testName],
                 div(class_='verdict')[verdict],
                 div(class_='summary')[
-                    'Summ'
+                    *summaryContent
                 ]
             ]
         ]
